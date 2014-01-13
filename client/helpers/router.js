23214b2132b82ae5636ae5a6e0a8af191d7961d8
michael verdi <michael.v.verdi@gmail.com>
2014-01-12 19:59:00 -0500

    adding loading screen

--- client/helpers/router.js
+++ client/helpers/router.js
@@ -14,6 +14,8 @@ Meteor.Router.filters({
   "requireLogin": function(page) {
     if (Meteor.user())
       return page;
+    else if (Meteor.loggingIn())
+      return "loading";
     else
       return "accessDenied";
   }
