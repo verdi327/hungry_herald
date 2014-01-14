a9180348aa3d218109beaae579205502a0609cfe
michael verdi <michael.v.verdi@gmail.com>
2014-01-13 19:32:49 -0500

    added permissions

--- collections/posts.js
+++ collections/posts.js
@@ -1,5 +1,17 @@
 Posts = new Meteor.Collection("posts");
 
+Posts.allow({
+  update: ownsDocument,
+  remove: ownsDocument
+});
+
+Posts.deny({
+  update: function(userId, post, fieldNames){
+    // may only edit the following three fields
+    return (_.without(fieldNames, "url", "title").length > 0);
+  }
+});
+
 Meteor.methods({
   post: function(postAttributes){
     var user = Meteor.user(),
