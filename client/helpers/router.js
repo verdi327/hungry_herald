c42d3091e7c3bea855d48030609992ef0979d9d0
michael verdi <michael.v.verdi@gmail.com>
2014-01-17 21:26:12 -0500

    set errors to seen after viewing to handle when to show and when to hide

--- client/helpers/router.js
+++ client/helpers/router.js
@@ -23,7 +23,13 @@ Meteor.Router.filters({
       return "loading";
     else
       return "accessDenied";
+  },
+
+  "clearErrors": function(page){
+    clearErrors();
+    return page;
   }
 });
 
 Meteor.Router.filter("requireLogin", {only: "postSubmit"});
+Meteor.Router.filter("clearErrors");
