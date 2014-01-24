637f9af52eb23679e30ba10a47301b68e4f79bbc
michael verdi <michael.v.verdi@gmail.com>
2014-01-24 16:55:25 -0500

    added load more button

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,5 +1,22 @@
 Template.postsList.helpers({
   posts: function(){
-    return Posts.find({}, {sort: {submitted: -1}});
+    return Posts.find({}, {sort: {submitted: -1}, limit: postsHandle.limit()});
+  },
+
+  postsReady: function() {
+    return ! postsHandle.loading();
+  },
+
+  allPostsLoaded: function() {
+    return ! postsHandle.loading() &&
+      Posts.find().count() < postsHandle.loaded();
+  }
+});
+
+
+Template.postsList.events({
+  "click .load-more": function(event) {
+    event.preventDefault();
+    postsHandle.loadNextPage();
   }
 });
