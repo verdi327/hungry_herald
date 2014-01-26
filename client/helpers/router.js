9a528ce70da19a72c0bbfedaf8a2e723ed155749
michael verdi <michael.v.verdi@gmail.com>
2014-01-25 19:08:18 -0500

    setting navigation header as active or not

--- client/helpers/router.js
+++ client/helpers/router.js
@@ -1,37 +1,35 @@
 Meteor.Router.add({
-  "/": {to: "bestPosts", as: "home"},
-  "/best": "bestPosts",
-  "/new": "newPosts",
+  '/': {to: 'newPosts', as: 'home'},
+  '/best': 'bestPosts',
+  '/new': 'newPosts',
 
-  "/posts/:_id": {
-    to: "postPage",
-    and: function(id) {Session.set("currentPostId", id);}
+  '/posts/:_id': {
+    to: 'postPage',
+    and: function(id) { Session.set('currentPostId', id); }
   },
 
-  "/posts/:_id/edit": {
-    to: "postEdit",
-    and: function(id) {Session.set("currentPostId", id);}
+  '/posts/:_id/edit': {
+    to: 'postEdit',
+    and: function(id) { Session.set('currentPostId', id); }
   },
 
-  "/submit": "postSubmit"
-
+  '/submit': 'postSubmit'
 });
 
 Meteor.Router.filters({
-  "requireLogin": function(page) {
+  'requireLogin': function(page) {
     if (Meteor.user())
       return page;
     else if (Meteor.loggingIn())
-      return "loading";
+      return 'loading';
     else
-      return "accessDenied";
+      return 'accessDenied';
   },
-
-  "clearErrors": function(page){
+  'clearErrors': function(page) {
     clearErrors();
     return page;
   }
 });
 
-Meteor.Router.filter("requireLogin", {only: "postSubmit"});
-Meteor.Router.filter("clearErrors");
+Meteor.Router.filter('requireLogin', {only: 'postSubmit'});
+Meteor.Router.filter('clearErrors');
